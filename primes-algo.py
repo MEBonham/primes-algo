06bdaa029bf7df55d12decad28ca92036969665f
MEBonham <programmer@mckaybonham.com>
2023-03-08 14:07:36 -0700

    Bugfixes, improved benchmark without sort

--- primes-algo.py
+++ primes-algo.py
@@ -26,18 +26,17 @@ def meb_sieve(limit):
             upgraded_factors = upgrade(composites[i])
             composites[prod(upgraded_factors)] = upgraded_factors
         else:
-            primes.append(i)
             if i > 2:
                 prime_steps[latest_p] = i
                 latest_p = i
             composites[i ** 2] = [i, i]
     
     primes = [2]
-    while len(primes) < len(prime_steps):
+    while len(primes) <= len(prime_steps):
         primes.append(prime_steps[primes[-1]])
     
     return primes
 
 t1 = datetime.now()
 p = meb_sieve(10000000)
-print(datetime.now() - t1, p[-1])       # less than 91 seconds on my smartphone
+print(datetime.now() - t1, p[-1])       # less than 83 seconds on my smartphone
