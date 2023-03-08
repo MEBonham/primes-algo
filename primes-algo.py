c1815d93327bb8c3543832a997622b7f01071673
MEBonham <programmer@mckaybonham.com>
2023-03-08 12:55:05 -0700

    Bug-hunting

--- primes-algo.py
+++ primes-algo.py
@@ -1,5 +1,6 @@
 from bisect import bisect
 from math import prod
+from datetime import datetime
 
 def meb_sieve(limit):
     
@@ -14,22 +15,30 @@ def meb_sieve(limit):
         f_copy[-1] = prime_steps[factors[-1]]
         return f_copy
     
-    latest_p = 0
+    latest_p = 2
     primes = []
     composites = {}
     
     for i in range(2, limit + 1):
+        print(i)
         if i in composites:
             expanded_factors = expand(composites[i])
+            print("expansion", prod(expanded_factors)
             composites[prod(expanded_factors)] = expanded_factors
    
-            upgraded_factors = upgrade(composites[i], primes)
+            upgraded_factors = upgrade(composites[i], primes
+            print("upgrade", prod(upgraded_factors)
             composites[prod(upgraded_factors)] = upgraded_factors
         else:
             primes.append(i)
             if i > 2:
                 prime_steps[latest_p] = i
                 latest_p = i
+                print(prime_steps)
             composites[i ** 2] = [i, i]
     
     return primes.sort()
+
+t1 = datetime.now()
+p = meb_sieve(10)
+print(datetime.now() - t1, p[-1])
